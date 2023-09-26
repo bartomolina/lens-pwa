99394de0e82d5aeafeac64796d5061a65661b1d6
Barto Molina <bartomolina@gmail.com>
2023-09-26 13:47:29 +0200

    feat: updated pwa metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,55 @@
 import "@/styles/globals.css";
 import "react-toastify/dist/ReactToastify.css";
 
+import { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { Client } from "./client";
 
+import {
+  APP_NAME,
+  APP_DEFAULT_TITLE,
+  APP_TITLE_TEMPLATE,
+  APP_DESCRIPTION,
+} from "@/lib/constants";
+
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "App",
-  description: "App",
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  themeColor: "#FFFFFF",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,6 @@
+export const APP_NAME = "Zupper";
+export const APP_DEFAULT_TITLE = "Zupper App";
+export const APP_TITLE_TEMPLATE = "%s - Zupper";
+export const APP_DESCRIPTION =
+  "Order food and drinks seamlessly. Pay with Crypto.";
 export const LENS_API = "https://api-mumbai.lens.dev/";
