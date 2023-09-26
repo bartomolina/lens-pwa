bec6319344476c74f62b1d0f4148f7cc10e1c31e
Barto Molina <bartomolina@gmail.com>
2023-09-26 14:08:31 +0200

    feat: updated pwa

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,5 @@
-export const APP_NAME = "Zupper";
-export const APP_DEFAULT_TITLE = "Zupper App";
-export const APP_TITLE_TEMPLATE = "%s - Zupper";
-export const APP_DESCRIPTION =
-  "Order food and drinks seamlessly. Pay with Crypto.";
+export const APP_NAME = "Lens PWA";
+export const APP_DEFAULT_TITLE = "Lens PWA App";
+export const APP_TITLE_TEMPLATE = "%s - Lens PWA";
+export const APP_DESCRIPTION = "Lens PWA starter App";
 export const LENS_API = "https://api-mumbai.lens.dev/";
