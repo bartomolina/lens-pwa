0be31ebe97b9113ad2f8fa9de1e6a98eb800de12
Barto Molina <bartomolina@gmail.com>
2023-10-02 21:50:13 +0200

    fix: testing login

--- app/test/page.tsx
+++
@@ -1,5 +0,0 @@
-"use client";
-
-export default function Test() {
-  return <div>test</div>;
-}
--- ui/v1/login.tsx
+++ ui/v1/login.tsx
@@ -1,13 +1,13 @@
 import { BlockTitle, List, ListButton } from "konsta/react";
 import { useRouter } from "next/navigation";
 
-import { useLogin } from "@/lib/lens/v1";
+// import { useLogin } from "@/lib/lens/v1";
 
 export function Login() {
   const router = useRouter();
-  const { mutate: login } = useLogin({
-    onSuccess: () => router.push("/feed"),
-  });
+  // const { mutate: login } = useLogin({
+  //   onSuccess: () => router.push("/feed"),
+  // });
 
   return (
     <>
@@ -16,7 +16,7 @@ export function Login() {
         <div className="text-right text-xs font-light">Mainnnet - Lens V1</div>
       </BlockTitle>
       <List strongIos insetIos>
-        <ListButton onClick={() => login()}>Log in</ListButton>
+        {/* <ListButton onClick={() => login()}>Log in</ListButton> */}
       </List>
     </>
   );
