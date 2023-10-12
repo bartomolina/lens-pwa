e9b3451fb297450928b21e9f5df9273c90bffa99
Barto Molina <bartomolina@gmail.com>
2023-10-12 03:54:13 +0200

    feat: remove family wallet

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,5 @@ const konstaConfig = require("konsta/config");
 module.exports = konstaConfig({
   content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./ui/**/*.{js,ts,jsx,tsx,mdx}"],
   plugins: [require("@tailwindcss/typography")],
+  darkMode: "class",
 });
