cc724b65c36dea1af745033915e40c8c05e50eed
Barto Molina <bartomolina@gmail.com>
2023-10-13 15:47:22 +0200

    fix: troubleshooting install prompt

--- app/client.tsx
+++ app/client.tsx
@@ -28,7 +28,6 @@ export function Client({ children }: { children: React.ReactNode }) {
   }, []);
 
   useEffect(() => {
-    alert("test:ismounted");
     isMounted(true);
   }, [isMounted]);
 
--- hooks/use-profile.ts
+++ hooks/use-profile.ts
@@ -6,13 +6,17 @@ export const useProfile = () => {
   return useQuery({
     queryKey: ["profile"],
     queryFn: async () => {
+      alert("test:fetching profile");
       const forProfileId = await lensClient.authentication.getProfileId();
+      alert("test:profile returned");
 
       if (forProfileId) {
+        alert("test:profile exists");
         const profile = await lensClient.profile.fetch({ forProfileId });
         console.log("use profile:", profile);
         return profile;
       } else if (forProfileId === null) {
+        alert("test:profile doesn't exist");
         return forProfileId;
       }
     },
