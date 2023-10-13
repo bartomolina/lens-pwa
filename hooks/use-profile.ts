c5ec8fa40961c3127e5e7804434e5806d3b7832a
Barto Molina <bartomolina@gmail.com>
2023-10-13 18:18:49 +0200

    fix: troubleshooting install prompt

--- hooks/use-profile.ts
+++ hooks/use-profile.ts
@@ -6,12 +6,17 @@ export const useProfile = () => {
   return useQuery({
     queryKey: ["profile"],
     queryFn: async () => {
+      alert("test:fetching");
       const forProfileId = await lensClient.authentication.getProfileId();
+      alert("test:fetched");
 
       if (forProfileId) {
+        alert(`test:exist ${forProfileId}`);
         const profile = await lensClient.profile.fetch({ forProfileId });
+        alert("test:returning");
         return profile;
       } else if (forProfileId === null) {
+        alert("test:does not exist");
         return forProfileId;
       }
     },
