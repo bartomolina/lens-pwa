aa07ceaf5919abe9735053b63a1641b82a1e8153
Barto Molina <bartomolina@gmail.com>
2023-10-13 14:33:13 +0200

    fix: troubleshooting install prompt

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
               </ListButton>
             )}
           </List>
-          <AddToHomeScreen />
+          {/* <AddToHomeScreen /> */}
           {isConnected && <Login />}
         </>
       )}
