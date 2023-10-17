43088c3902ba075f8905fe3653a778c885914bfc
Barto Molina <bartomolina@gmail.com>
2023-10-17 10:57:19 +0200

    fix: form validation

--- hooks/use-create-publication.ts
+++ hooks/use-create-publication.ts
@@ -35,6 +35,7 @@ export const useCreatePublication = ({
 
           const resData = await res.json();
 
+          alert(`test:uploading${content}end`);
           metadata = image({
             image: {
               item: `ipfs://${resData.IpfsHash}`,
@@ -69,11 +70,14 @@ export const useCreatePublication = ({
 
         const resData = await res.json();
 
+        alert(`test:posting`);
         const postResult = await lensClient.publication.postOnMomoka({
           contentURI: `ipfs://${resData.IpfsHash}`,
         });
+        alert(`test:done`);
 
         if ("reason" in postResult && typeof postResult.reason === "string") {
+          alert(`test:error${postResult.reason}`);
           throw new Error(postResult.reason);
         }
 
