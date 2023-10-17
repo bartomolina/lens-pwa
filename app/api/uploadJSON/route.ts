d381a123ecb2afccdd4b68dfbdd69df02e37ab98
Barto Molina <bartomolina@gmail.com>
2023-10-17 11:14:51 +0200

    fix: form validation

--- app/api/uploadJSON/route.ts
+++ app/api/uploadJSON/route.ts
@@ -1,6 +1,9 @@
 export async function POST(req: Request) {
   const formData = await req.formData();
   const json = formData.get("message");
+  if (!json) {
+    return Response.json({ success: false });
+  }
 
   const res = await fetch("https://api.pinata.cloud/pinning/pinJSONToIPFS", {
     method: "POST",
