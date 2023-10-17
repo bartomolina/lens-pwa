ea9382fabf32d883359b6bbed55704cf72edd8da
Barto Molina <bartomolina@gmail.com>
2023-10-17 11:23:43 +0200

    chore: removed test logging

--- app/api/uploadJSON/route.ts
+++ app/api/uploadJSON/route.ts
@@ -1,12 +1,9 @@
 export async function POST(req: Request) {
   const formData = await req.formData();
   const json = formData.get("message");
-  console.log("test:uploadingjson", json);
-  console.log("test:uploadingjson", formData);
   if (!json) {
     return Response.json({ success: false });
   }
-  console.log("test:uploadingjson:done");
 
   const res = await fetch("https://api.pinata.cloud/pinning/pinJSONToIPFS", {
     method: "POST",
@@ -16,8 +13,6 @@ export async function POST(req: Request) {
     },
     body: json,
   });
-  console.log("test:uploadingjson:done2", res);
   const resData = await res.json();
-  console.log("test:uploadingjson:done3", resData);
   return Response.json(resData);
 }
